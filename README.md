0e7cb353911943b72eb613a889ffbf781f4ae079
ISDL29 <ivansaizdelucasso@gmail.com>
2022-11-11 22:04:34 +0100

    creación de documentación

---
+++ README.md
@@ -0,0 +1,43 @@
+# background
+## Clasificación de la calidad del aire
+El Acuerdo de París es un tratado internacional sobre el cambio climático que fue adoptado por 196 Partes en la COP21 de París. Su objetivo es limitar el calentamiento mundial muy por debajo de 2, preferiblemente a 1,5 grados centígrados, en comparación con los niveles preindustriales.
+
+Para alcanzar este objetivo de temperatura a largo plazo, los países se proponen alcanzar el máximo de las emisiones de gases de efecto invernadero lo antes posible para lograr un planeta con clima neutro para mediados de siglo.
+
+Es por ello que la Unión Europea esta destinando grandes cantidades de recursos al desarrollo de nuevas tecnologías que permitan la mejorar la lucha contra la contaminación. Una de estas es un nuevo tipo de sensor basado en tecnología láser que permita detectar la calidad del aire en función de diferentes mediciones.
+
+# problem
+
+## Predicción de la calidad del aire
+
+El sensor mide 8 características del aire. A partir de esas características, se tiene que determinar la calidad del aire, clasificada en 3 categorías (buena mala y regular).
+
+El dataset consta de 2 archivos:
+
+- **train.csv**: Este dataset contiene tanto las variables predictoras como el tipo de clasificación de calidad del aire.
+
+- **test.csv**: Este dataset contiene las variables predictoras con las que se tendrá que predecir el tipo de calidad de aire.
+
+La estructura del dataset es la siguiente:
+
+- **Features**: El dataset contiene 8 features en 8 columnas que son los parámetros medidos por los diferentes sensores. Estos corresponden a las diferentes interacciones que han tenido los haces de los láseres al travesar las partículas del aire.
+
+- **Target**: El target corresponde al 'label' que clasifica la calidad del aire.
+
+  - Target **0** corresponde a una calidad del aire **Buena**
+  - Target **1** corresponde a una calidad del aire **Moderada**
+  - Target **2** corresponde a una calidad del aire **Peligrosa**
+
+# results
+
+Ver Jump2Digital2022.ipynb
+
+# analysis
+
+Ver Jump2Digital2022.ipynb
+
+# solution
+
+Ver Jump2Digital2022.ipynb
+
+# license
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-# This script only generates the result.
-# The full analysis can be found on Jump2Digital2022.ipynb
+# Este código solo genera la solución.
+# El análisis completo se encuentra en Jump2Digital2022.ipynb
 
-# IMPORTS ---------------------------------------------------------------------
+# IMPORTACIONES ---------------------------------------------------------------
 
 import os
 import pandas as pd
@@ -12,12 +12,14 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
 
-# DATA INGESTION --------------------------------------------------------------
+# INGESTA DE DATOS ------------------------------------------------------------
 
 ROOT=os.getcwd()
 TRAIN_FILE=os.path.join(ROOT,'train.csv')
 TEST_FILE=os.path.join(ROOT,'test.csv')
-PRED_FILE=os.path.join(ROOT,'predictions.json')
+PRED_JSON_FILE=os.path.join(ROOT,'predictions.json')
+PRED_CSV_FILE=os.path.join(ROOT,'predictions.csv')
+CV_RES_FILE=os.path.join(ROOT,'cv_results.csv')
 
 train=pd.read_csv(TRAIN_FILE, sep=';')
 X_train=train.drop('target',axis=1)
@@ -25,13 +27,13 @@ y_train=train['target']
 
 X_test=pd.read_csv(TEST_FILE, sep=';')
 
-# MODEL SETUP -----------------------------------------------------------------
+# CONFIGURACIÓN DEL MODELO ----------------------------------------------------
 
-# Defining the 'do nothing' transformer.
+# Definiendo el transformador neutro.
 class Identity(BaseEstimator, TransformerMixin):
-    '''This transformer does not make any changes to data.
-    As we want to choose between dropping bad features or not in GridSearchCV, 
-    we need a transformer option that does nothing to data'''
+    '''Este transformador deja intacto los datos. Su utilidad es proporcionar
+    una opción en la que no se eliminan las características 7 y 8 en 
+    GridSearchCV.'''
     def __init__(self):
         pass  
     def fit(self, input_array, y=None):
@@ -39,25 +41,25 @@ class Identity(BaseEstimator, TransformerMixin):
     def transform(self, input_array, y=None):
         return input_array*1
 
-# Features to drop according to the analysis on Jump2Digital2022.ipynb    
+# Característica a eliminar según el análisis en Jump2Digital2022.ipynb    
 dropfeats=['feature7','feature8']
 
-# Do nothing transformer
+# Transformador neutro
 identity=Identity()
 
-# Drop bad features transformer
+# Transformador que elimina las características 7 y 8
 drop=ColumnTransformer(transformers=[('dropfts','drop',dropfeats)],
                        remainder='passthrough')
 rf=RandomForestClassifier(random_state=2022)
 
-# Pipeline setup.
-# 1. Preprocessing: Drop features or not. Default option is doing nothing.
-# 2. Random forest classifier.
+# Configuración del pipeline
+# 1. Preprocesamiento: No tocar los datos
+# 2. Clasificador random forest
 pipeline=Pipeline([('pre',identity),('rf',rf)])
 
-# Model grid
-# 1. Whether drop bad features or not.
-# 2. Random forest hyperparameters options.
+# Opciones de configuración del modelo
+# 1. Eliminación de características 7 y 8 o no hacer nada
+# 2. Hiperparámetros del random forests a explorar
 pipe_grid={'pre':[identity,drop],
            'rf__n_estimators':[100,150,200],
            'rf__min_samples_split':[2,3,4,5],
@@ -65,14 +67,25 @@ pipe_grid={'pre':[identity,drop],
            'rf__class_weight':['balanced',None],
            }
 
-# Model initialization.
-clf=GridSearchCV(pipeline,param_grid=pipe_grid,verbose=0,cv=7)
+# Inicialización del modelo
+clf=GridSearchCV(pipeline,
+                 param_grid=pipe_grid,
+                 return_train_score=True,
+                 verbose=0,
+                 cv=7
+                 )
 
-# PREDICTION ------------------------------------------------------------------
+# PREDICCIÓN ------------------------------------------------------------------
 
-# Model training and prediction
+# Entrenamiento del modelo y predicción
 clf.fit(X_train,y_train)
 y_test=pd.DataFrame(clf.predict(X_test),columns=['target'])
 
-# Saving predictions in a json file.
-y_test.to_json(PRED_FILE,indent=2)
+# Guardado de los resultados de la exploración de hiperparámetros
+clf.cv_results_.to_csv(CV_RES_FILE)
+
+# Guardado de las predicciones en formato json y csv
+y_test.to_json(PRED_JSON_FILE,indent=2)
+y_test.to_csv(PRED_CSV_FILE,index=False)
+
+# =============================================================================
